523d60c9af3cfdfffd418a859e25e458d50bb3c5
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-06-01 15:52:04 -0400

    Create dfionelogo.tsx

---
+++ packages/app/components/dfionelogo.tsx
@@ -0,0 +1,15 @@
+import cls from "./GnosisLogo.module.css"
+
+const QoneLogo: React.FC = () => (
+  <div className={cls.container}>
+    <img
+      className={cls.logo}
+      src="/qlogo.svg"
+      alt="DFI1 Logo"
+      height={36}
+      width={36}
+    />
+  </div>
+)
+
+export default Dfionelogo
