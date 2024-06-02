14c6c2da9ed5f4474adee7c791c0eb19a8fb42ea
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-06-02 02:54:28 -0400

    Update config.ts

--- packages/app/config.ts
+++ packages/app/config.ts
@@ -1,18 +1,19 @@
 import { Chain, mainnet, gnosis, goerli } from "wagmi/chains"
 
-export const LOCKED_TOKEN_NAME = "Gnosis"
-export const LOCKED_TOKEN_SYMBOL = "GNO"
-export const CLAIM_TOKEN_NAME = "Locked Gnosis"
-export const CLAIM_TOKEN_SYMBOL = "LGNO"
+export const LOCKED_TOKEN_NAME = "Da-Fi"
+export const LOCKED_TOKEN_SYMBOL = "Da-Fi"
+export const CLAIM_TOKEN_NAME = "Locked Da-Fi"
+export const CLAIM_TOKEN_SYMBOL = "LDFI"
 
 // used for price lookup
-export const COINGECKO_TOKEN_ID = "gnosis"
+export const COINGECKO_TOKEN_ID = "da-fi"
 
 export const CHAINS = [mainnet, gnosis, goerli] as Chain[]
 
 export const CONTRACT_ADDRESSES: { [chainId: number]: string } = {
   1: "0x4f8AD938eBA0CD19155a835f617317a6E788c868",
-  100: "0xd4Ca39f78Bf14BfaB75226AC833b1858dB16f9a1",
+"0xeaaD65885fEA47a3B1258935f4Ce83aaB06FDD3A", 
+100: "0xd4Ca39f78Bf14BfaB75226AC833b1858dB16f9a1",
   5: "0xCfDf830D59cbb30197b78dB578066F6AbD8e4b3b", //"0xB5d2b6cD4b0417D566593B42B21F2316FDCCB5FA",
 
   // 4: "0x01FD5975E40D16838a7213e2fdfFbBBA4477c14d", // deposit period ongoing
